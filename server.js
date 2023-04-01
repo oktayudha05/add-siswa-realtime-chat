e8830f555bb004d7daac1b6ece88fd8863be9364
oktayudha05 <oktayudha05@gmail.com>
2023-04-01 18:38:07 +0700

    add view engine

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mysql = require('mysql')
 
 const app = express()
 
+app.set("view engine", "ejs")
+app.set("views", "views")
+
 const db = mysql.createConnection({
     host : "localhost",
     database : "db_realtimechat",
@@ -12,10 +15,16 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
     if (err) throw err
-    console.log('database connected...');
-    app.get("/", (req, res) => {
-        res.send('ROUTE OPEN')
-    })
+    console.log('database connected...')
+
+    const sql = "SELECT * FROM user"
+    db.query(sql, (err, result) => {
+        const users = JSON.parse(JSON.stringify(result))
+        console.log('hasil database ->', users);
+        app.get("/", (req, res) => {
+            res.render("index", {users : users, title : 'OMYMA14 ROOM CHAT'})
+        })
+    }) 
 })
 
 app.listen(8000, () => {
