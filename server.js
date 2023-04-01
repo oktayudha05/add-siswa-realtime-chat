b763bb01b87ecec48d59d3e59c36dce7758d6903
oktayudha05 <oktayudha05@gmail.com>
2023-04-02 05:28:52 +0700

    final

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const mysql = require('mysql')
+const BodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express()
+
+app.use(BodyParser.urlencoded({extended: true}))
 
 app.set("view engine", "ejs")
 app.set("views", "views")
@@ -16,15 +20,24 @@ const db = mysql.createConnection({
 db.connect((err) => {
     if (err) throw err
     console.log('database connected...')
-
-    const sql = "SELECT * FROM user"
-    db.query(sql, (err, result) => {
-        const users = JSON.parse(JSON.stringify(result))
-        console.log('hasil database ->', users);
-        app.get("/", (req, res) => {
+    
+    app.get("/", (req, res) => {
+        const sql = "SELECT * FROM user"
+        db.query(sql, (err, result) => {
+            const users = JSON.parse(JSON.stringify(result))
             res.render("index", {users : users, title : 'OMYMA14 ROOM CHAT'})
+        }) 
+    })
+
+    app.post("/tambah", (req, res) => {
+        const insertSql = `INSERT INTO user (nama, kelas) VALUES ('${req.body.nama}', '${req.body.kelas}')`
+        db.query(insertSql, (err, result) => {
+            if (err) throw err
+            res.redirect("/")
         })
-    }) 
+    })
+
+    
 })
 
 app.listen(8000, () => {
